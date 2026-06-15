4f813bf7dc1e026b029e5608ee653adb466dd273
alzaaa6 <marc.alzamora.02@gmail.com>
2026-06-15 13:38:35 +0200

    Petita modificacio

--- SCRIPTS/options.gd
+++ SCRIPTS/options.gd
@@ -11,5 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_btn_volver_pressed() -> void:
+func _on_btn_tornar_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://SCENES/PantallaInici.tscn")
