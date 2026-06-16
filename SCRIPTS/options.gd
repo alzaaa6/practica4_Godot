db15234f62dd6edff583b61390194d731aef1425
Marc <marc.alzamora.02@gmail.com>
2026-06-16 19:58:34 +0200

    Musica afegida

--- SCRIPTS/options.gd
+++ SCRIPTS/options.gd
@@ -1,15 +1,37 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Carrega els valors actuals dels busos d'àudio
+	%HSliderMaster.value = db_to_linear(
+		AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
+	) * 100.0
+	%HSliderMusic.value = db_to_linear(
+		AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music"))
+	) * 100.0
+	%HSliderSFX.value = db_to_linear(
+		AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX"))
+	) * 100.0
 
+func _on_master_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(
+		AudioServer.get_bus_index("Master"),
+		linear_to_db(value / 100.0)
+	)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_music_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(
+		AudioServer.get_bus_index("Music"),
+		linear_to_db(value / 100.0)
+	)
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(
+		AudioServer.get_bus_index("SFX"),
+		linear_to_db(value / 100.0)
+	)
+
 func _process(delta: float) -> void:
 	pass
-
 
 func _on_btn_tornar_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://SCENES/PantallaInici.tscn")
