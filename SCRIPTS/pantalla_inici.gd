60fb866ffd17eb10aa3a54ad178928918d8ab1f1
Marc <marc.alzamora.02@gmail.com>
2026-06-19 13:42:20 +0200

    Selector personatge fet

--- SCRIPTS/pantalla_inici.gd
+++ SCRIPTS/pantalla_inici.gd
@@ -4,18 +4,34 @@ extends Control
 @onready var fade: ColorRect = $Fade
 @onready var anim: AnimationPlayer = $Fade/AnimationPlayer
 @onready var sfx_jugar: AudioStreamPlayer2D = $SFXPlay
+@onready var btn_esquerra: Button = $HBoxContainer/BtnEsquerra
+@onready var btn_dreta: Button = $HBoxContainer/BtnDreta
+@onready var personatge_img: TextureRect = $HBoxContainer/TextureRect
+@onready var label_nom: Label = $LabelNom
+
+var noms := ["Guerrer", "Mag", "Archer"]
 
 
+var personatge_actual := 0
+var personatges := [
+	preload("res://ASSETS/personatge1.png"),
+	preload("res://ASSETS/personatge2.png"),
+	preload("res://ASSETS/personatge3.png"),
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	actualitzar_personatge()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+
+func actualitzar_personatge() -> void:
+	personatge_img.texture = personatges[personatge_actual]
+	label_nom.text = noms[personatge_actual]
 
 func _on_btn_options_pressed() -> void:
 	sfx_player.play()
@@ -34,3 +50,12 @@ func _on_btn_sortir_pressed() -> void:
 	sfx_player.play()
 	await sfx_player.finished
 	get_tree().quit();
+
+
+func _on_btn_esquerra_pressed() -> void:
+	personatge_actual = (personatge_actual - 1 + personatges.size()) % personatges.size()
+	actualitzar_personatge()
+
+func _on_btn_dreta_pressed() -> void:
+	personatge_actual = (personatge_actual + 1) % personatges.size()
+	actualitzar_personatge()
